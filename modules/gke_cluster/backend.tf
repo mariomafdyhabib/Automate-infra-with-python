72359abb0341aea9313c67006f8db4d79cc83dc9
mariomafdyhabib <mariomafdy333@gmail.com>
2025-10-07 11:23:18 +0300

    test

--- modules/gke_cluster/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket      = "mariomafdy-bucket"   # <-- replace with your bucket name
-    prefix      = "terraform/state"             # folder path inside the bucket
-    # credentials = "gcp-terraform-key.json"      # service account key file
-  }
-}
